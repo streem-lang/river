7af22a3fc1b8428e26d37a09ef0bd63393fbfc27
christopherdumas <christopherdumas@me.com>
2014-12-24 10:51:04 -0800

    Sort of working but being replaced by git process.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func help() {
@@ -13,7 +14,7 @@ func help() {
 	cyan := color.New(color.FgCyan).SprintFunc()
 
 	fmt.Printf("river usage: river %s %s\n", cyan("[command]"), yellow("[arguments ...]"))
-	fmt.Printf("    river %s %s           Installs streem package with same name\n", cyan("install"), yellow("author/name"))
+	fmt.Printf("    river %s %s           Installs streem package with same name\n", cyan("install"), yellow("author name"))
 	fmt.Printf("    river %s                       Installs streem package that you are in\n", cyan("install"))
 	fmt.Printf("    river %s %s                   Removes streem package that you name\n", cyan("remove"), yellow("name"))
 	fmt.Printf("    river %s                         Interactive prompt to setup your project\n", cyan("setup"))
@@ -33,11 +34,22 @@ func main() {
 	switch args[0] {
 	case "install":
 		fmt.Println("Creating package directory..")
-		if len(args) == 2 {
-			dir := "/usr/local/bin/river-pkgs/" + args[1]
+		if len(args) == 3 {
+			dir := "/usr/local/bin/river-pkgs/" + args[2]
 			err := os.MkdirAll(dir, 0777)
 			if err == nil {
 				fmt.Println("Downloading package from servers....")
+
+				binary, err1 := exec.LookPath("git")
+				if err1 != nil {
+					log.Fatal(err1)
+				}
+
+				args := []string{"git", "clone", "http://github.com/" + args[1] + "/" + args[2], dir}
+				err2 := syscall.Exec(binary, args, os.Environ())
+				if err2 != nil {
+					log.Fatal(err2)
+				}
 				color.Green("Done!")
 			} else {
 				color.Red("ERROR!")
